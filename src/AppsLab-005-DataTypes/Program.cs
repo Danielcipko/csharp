4b966a26be00ae39481f8196626799149f07e2d4
cipkod25 <cipkod25@Z00T1-10>
2025-09-10 13:09:17 +0200

    10.9 vsetko

--- src/AppsLab-005-DataTypes/Program.cs
+++ src/AppsLab-005-DataTypes/Program.cs
@@ -1,2 +1,84 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace MyApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        { 
+            /*
+            int myAge = 15;
+            Console.WriteLine(myAge);
+
+            float pi = 3.14f;
+            Console.WriteLine(pi);
+
+            string name = "Daniel";
+            Console.WriteLine(name);
+
+            char gender = 'M';
+            Console.WriteLine(gender);
+
+            bool isAdult = false;
+            Console.WriteLine(isAdult); */
+            
+
+            //scitavanie A B 
+            int a = 6; 
+            int b = 7;
+            int sum = a + b;
+            Console.WriteLine(sum);
+        
+
+            Console.WriteLine(a - b);
+            //odcitavanie
+            int sum2 = a - b;
+            Console.WriteLine(sum2);
+
+
+            //nasobenie delenie 
+            Console.WriteLine(a * b);
+            Console.WriteLine(a / b);
+
+            Console.WriteLine("Daniel");
+
+            int birthday = 12;
+            int birthMonth = 8;
+            int birthYear = 2010;
+
+            int birthsum = birthday + birthMonth + birthYear;
+
+            Console.WriteLine(birthsum);
+
+            Console.WriteLine(birthsum * 10);
+            Console.WriteLine((birthday+birthMonth) * birthYear);
+            Console.WriteLine(5 / 3);
+            Console.WriteLine(5f / 3f);
+
+            string Name = "Daniel";
+            int bYear = 2010;
+
+
+
+
+
+            
+
+
+
+
+        }
+
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
