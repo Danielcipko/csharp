ff242745bf42b8011248a15ecb8d454b79912cd9
Danielcipko <daniel.cipko10@gmail.com>
2025-12-12 15:47:56 +0100

    Moja Banka

--- BANKA/Account.cs
+++ BANKA/Account.cs
@@ -3,14 +3,14 @@ using System.Collections.Generic;
 
 namespace BANKA
 {
-    public class Ucet
+    public class Account
     {
          public int ID { get; set; }
          public string MenoPriezvisko { get; set; }
          public string Heslo { get; set; }
          public decimal Zostatok { get; set; }
          public List<string> HistoriaPrevodov { get; set; }
-         public Ucet(int id, string menoPriezvisko, string heslo, decimal pociatocnyZostatok)
+         public Account(int id, string menoPriezvisko, string heslo, decimal pociatocnyZostatok)
          {
             ID = id;
             MenoPriezvisko = menoPriezvisko;
