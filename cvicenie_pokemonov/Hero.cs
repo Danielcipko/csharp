8fa867dd9502f492fd9a9a4f4f829f4266460f64
cipkod25 <cipkod25@Z00T1-10>
2026-03-04 12:42:14 +0100

    pokemoni

---
+++ cvicenie_pokemonov/Hero.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cvicenie_pokemonov
+{
+    public class Hero
+    {
+        public int Health { get; set; } //zobrazuje aktualne zivoty hrdinu
+        public int HealthMax { get; set; } //zobrazuje jeho maximalne HP
+        public int Damage { get; set; }
+
+        public Hero(int health, int healthMax, int damage)
+        {
+            Health = health;
+            HealthMax = healthMax;
+            Damage = damage;
+        }
+    }
+}
+
+
