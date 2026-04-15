9dd3fdbc55ed7d6aac632dc18f7a511aee0eed8f
Danielcipko <daniel.cipko10@gmail.com>
2026-04-15 10:51:54 +0200

    rnd clothing

--- RND clothing e-shop/ShopPage.xaml.cs
+++ RND clothing e-shop/ShopPage.xaml.cs
@@ -12,32 +12,58 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
 
+using System;
+using System.Collections.Generic;
+using System.Windows;
+
 namespace RND_clothing_e_shop
 {
-    /// <summary>
-    /// Interaction logic for ShopPage.xaml
-    /// </summary>
     public partial class ShopPage : Window
     {
+        
+        public static List<Produkt> KosikList = new List<Produkt>();
+
         public ShopPage()
         {
             InitializeComponent();
         }
+
         private void BackButton_Click(object sender, RoutedEventArgs e)
-        { 
+        {
             MainWindow mainWindow = new MainWindow();
             mainWindow.Show();
-
             this.Close();
         }
-        private void CartButton_Click(object sender, RoutedEventArgs e) 
-        { 
+
+        private void CartButton_Click(object sender, RoutedEventArgs e)
+        {
             KosikWindow kosikWindow = new KosikWindow();
             kosikWindow.Show();
-
             this.Close();
         }
 
+        private void AddToCart(string name, decimal price)
+        {
+            
+            Produkt novyProdukt = new Produkt
+            {
+                Name = name,
+                Price = price
+            };
+
+           
+            KosikList.Add(novyProdukt);
+
+            MessageBox.Show($"{name} bol pridaný do košíka!");
+        }
+
+       
+        private void AddWhiteShirt_Click(object sender, RoutedEventArgs e) => AddToCart("Biele Tričko", 19.99m);
+        private void AddBlackHoodie_Click(object sender, RoutedEventArgs e) => AddToCart("Čierna Mikina", 39.99m);
+        private void AddJeans_Click(object sender, RoutedEventArgs e) => AddToCart("Rifle", 49.99m);
+        private void AddJacket_Click(object sender, RoutedEventArgs e) => AddToCart("Bunda", 89.99m);
+
+        
         private void AllCategory_Click(object sender, RoutedEventArgs e) { }
         private void TrickaCategory_Click(object sender, RoutedEventArgs e) { }
         private void MikinyCategory_Click(object sender, RoutedEventArgs e) { }
@@ -45,10 +71,5 @@ namespace RND_clothing_e_shop
         private void BundyCategory_Click(object sender, RoutedEventArgs e) { }
         private void TopankyCategory_Click(object sender, RoutedEventArgs e) { }
         private void DoplnkyCategory_Click(object sender, RoutedEventArgs e) { }
-
-        private void AddWhiteShirt_Click(object sender, RoutedEventArgs e) { }
-        private void AddBlackHoodie_Click(object sender, RoutedEventArgs e) { }
-        private void AddJeans_Click(object sender, RoutedEventArgs e) { }
-        private void AddJacket_Click(object sender, RoutedEventArgs e) { }
     }
 }
